5028c2ccd6f95ecf52b05b3eca4bcb7427b72ca5
Maximilian Schreiter <maximilian.schreiter@t-online.de>
2021-06-23 16:36:00 +0200

    Outsource validators

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,22 +20,24 @@ import * as siaSchema from './analyzer/config/config.schema.json';
 import { cleanTempFiles, IFile, getOrCreateTempFile } from "./analyzer/handlers/file/index";
 import { JSONS, PACKAGE_JSON, SIARC, TYPESCRIPT } from './analyzer/utils';
 
-const connection = createConnection(ProposedFeatures.all);
-const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+import { Validator } from "./analyzer/handlers/validator";
+
+export const connection = createConnection(ProposedFeatures.all);
+export const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
 
 let jsonLanguageService: LanguageService;
 
 const configValidator: ConfigValidator = new ConfigValidator();
 const analyzer: Analyzer = new Analyzer();
 
+let validator: Validator;
+
 const pendingValidations: { [uri: string]: NodeJS.Timer } = {};
+
+// TODO was macht bewirkt das?
 const validationDelay = 300;
 
 connection.onInitialize(async (params: InitializeParams) => {
-  jsonLanguageService = getLanguageService({
-    clientCapabilities: params.capabilities,
-  });
-
   const result: InitializeResult = {
     capabilities: {
       completionProvider: {
@@ -45,15 +47,17 @@ connection.onInitialize(async (params: InitializeParams) => {
     },
   };
 
+  validator = new Validator(params);
+
   // Load package.json and .siarc.json, if they exists
   if (params.initializationOptions) {
     if (params.initializationOptions.siarcTextDoc) {
       const siarc = params.initializationOptions.siarcTextDoc;
       const textDoc = TextDocument.create(siarc.uri, siarc.languageId, siarc.version, siarc.content);
-      await validateConfig(textDoc);
+      await validator.validateConfig(textDoc);
     }
     if (params.initializationOptions.packageJson) {
-      loadPackageJson(params.initializationOptions.packageJson);
+      validator.loadPackageJson(params.initializationOptions.packageJson);
     }
   }
   return result;
@@ -63,19 +67,19 @@ connection.onInitialize(async (params: InitializeParams) => {
 // TODO change rest to async stuff
 
 documents.onDidOpen(async (event) => {
-  checkForValidation(event.document);
+  validator.checkForValidation(event.document);
 });
 
 documents.onDidSave(async (event) => {
-  checkForValidation(event.document);
+  validator.checkForValidation(event.document);
 });
 
 documents.onDidChangeContent(async (event) => {
-  checkForValidation(event.document);
+  validator.checkForValidation(event.document);
 });
 
 documents.onDidClose(async (event) => {
-  cleanPendingValidations(event.document.uri);
+  validator.cleanPendingValidations(event.document.uri);
   if (event.document.languageId === TYPESCRIPT.LANGUAGE_ID) {
     cleanTempFiles(event.document.uri).then((fileUri) => {
       console.debug(`Removed file at ${fileUri}`);
@@ -103,115 +107,6 @@ connection.onHover(async (textDocumentPosition: HoverParams, token: Cancellation
     return null; // TODO: Maybe give a documentation of the endpoint
   }
 });
-
-function cleanPendingValidations(uri: string): void {
-  const request = pendingValidations[uri];
-  if (request) {
-    clearTimeout(request);
-    delete pendingValidations[uri];
-  }
-}
-
-function triggerConfValidation(document: TextDocument): void {
-  cleanPendingValidations(document.uri);
-  pendingValidations[document.uri] = setTimeout(async () => {
-    delete pendingValidations[document.uri];
-    await validateConfig(document);
-  }, validationDelay);
-}
-
-function triggerTypescriptValidation(document: TextDocument, file: IFile): void {
-  cleanPendingValidations(file.fileUri);
-  pendingValidations[file.fileUri] = setTimeout(() => {
-    delete pendingValidations[file.fileUri];
-    validateTypescript(document, file);
-  }, validationDelay);
-}
-
-async function checkForValidation(document: TextDocument): Promise<void> {
-  switch (document.languageId) {
-    case TYPESCRIPT.LANGUAGE_ID: {
-      getOrCreateTempFile(document).then((file) => {
-        triggerTypescriptValidation(document, file);
-      }).catch((reason) => {
-        return;
-      });
-      break;
-    }
-    case JSONS.LANGUAGE_ID: {
-      validateJson(document);
-      break;
-    }
-    default: {
-      return;
-    }
-  }
-}
-
-function validateJson(document: TextDocument) {
-  if (document.uri.endsWith(SIARC)) {
-    triggerConfValidation(document);
-  } else if (document.uri.endsWith(PACKAGE_JSON)) {
-    loadPackageJson(document.getText());
-    // Revalidate all typescript files
-    documents.all().forEach((doc: TextDocument) => {
-      checkForValidation(doc);
-    });
-  }
-}
-
-async function validateConfig(document: TextDocument): Promise<void> {
-  const jsonDoc = jsonLanguageService.parseJSONDocument(document);
-
-  const syntaxErrors = await jsonLanguageService.doValidation(document, jsonDoc, { schemaValidation: 'error', trailingCommas: 'error' }, siaSchema as JSONSchema);
-  const semanticErrors = configValidator.validateConfigSemantic(document, jsonDoc);
-
-  if (syntaxErrors.length === 0 && semanticErrors.length === 0) {
-    analyzer.config = document.getText();
-    connection.sendDiagnostics({ uri: document.uri, diagnostics: [] });
-    documents.all().forEach(async (doc: TextDocument) => {
-      if (doc.languageId === TYPESCRIPT.LANGUAGE_ID) {
-        checkForValidation(doc);
-      }
-    });
-  } else {
-    connection.sendDiagnostics({ uri: document.uri, diagnostics: semanticErrors });
-  }
-}
-
-function validateTypescript(document: TextDocument, file: IFile): void {
-  const diagnostics: Diagnostic[] = [];
-
-  const version = document.version;
-  analyzer.analyzeEndpoints(file).forEach((error: SemanticError) => {
-    diagnostics.push({
-      message: error.message,
-      range: {
-        start: document.positionAt(error.position.start),
-        end: document.positionAt(error.position.end),
-      },
-      severity: DiagnosticSeverity.Error,
-    });
-  });
-
-  setImmediate(() => {
-    // To be clear to send the correct diagnostics to the current document
-    const currDoc = documents.get(document.uri);
-    if (currDoc && currDoc.version === version) {
-      connection.sendDiagnostics({ uri: document.uri, diagnostics });
-    }
-  });
-}
-
-function loadPackageJson(json: string) {
-  if (json) {
-    const pack = JSON.parse(json);
-    if (pack.name) {
-      analyzer.currentService = pack.name;
-    }
-    analyzer.detectFrameworkOrLibrary(pack);
-  }
-}
 
 documents.listen(connection);
 connection.listen();
