2aa20b4b297ebed0dcf0d0a3e68396932e8b2b5c
Maximilian Schreiter <maximilian.schreiter@t-online.de>
2021-11-26 16:48:10 +0100

    Add docu for autoCompletionService

--- server/src/analyzer/siarc/service/AutoCompletionService.ts
+++ server/src/analyzer/siarc/service/AutoCompletionService.ts
@@ -6,15 +6,19 @@ export class AutoCompletionService {
   completionItems: CompletionItem[] = [];
 
   constructor() {}
+
   /**
-   * provideCompletionItems
+   * Provides the specific completionsitems for an backend.
+   * @param params
+   * @param token
+   * @param currentConfigs siarc configs
+   * @returns
    */
   public provideCompletionItems(params: CompletionParams, token: CancellationToken, currentConfigs: ServiceConfig[]): CompletionItem[] {
     this.completionItems.forEach((completionItem) => {
       let endpoint: Endpoint | undefined;
       currentConfigs.forEach((serviceConfig) => {
         endpoint = serviceConfig?.endpoints.find((endpoint) => {
-          6;
           if (endpoint.path === completionItem.filterText) {
             return endpoint;
           }
@@ -29,6 +33,11 @@ export class AutoCompletionService {
     return this.completionItems;
   }
 
+  /**
+   * Generates completion items from siarc.json.
+   * @param currentConfig siarc.json config
+   * @returns List of completion items
+   */
   public generateCompletionItems(currentConfig: ServiceConfig): CompletionItem[] {
     const completionItems: CompletionItem[] = [];
     connection.console.log('Generate completion ' + currentConfig.endpoints);
@@ -41,6 +50,12 @@ export class AutoCompletionService {
     return completionItems;
   }
 
+  /**
+   * Creates a completionItem
+   * @param endpoint Endpoints
+   * @param currentConfig Siarc.json
+   * @returns A completionitem
+   */
   private createEndpointCompletionItem(endpoint: Endpoint, currentConfig: ServiceConfig): CompletionItem {
     const label: string = endpoint.path.toUpperCase();
 
